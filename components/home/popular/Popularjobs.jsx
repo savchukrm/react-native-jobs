d3830c70736f38772ae7f0b4400944f689fc791c
Rymma Savchuk <savchukrmm@gmail.com>
2023-05-24 19:22:26 +0300

    welcome and popularjobs components were updated

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,14 +1,43 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import { useState } from 'react';
+import { useRouter } from 'expo-router';
 
-import styles from './popularjobs.style'
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  FlatList,
+  ActivityIndicator,
+} from 'react-native';
+
+import styles from './popularjobs.style';
+import { COLORS, SIZES } from '../../../constants';
+import PopularJobsCard from '../../common/cards/popular/PopularJobCard';
 
 const Popularjobs = () => {
-  return (
-    <View>
-      <Text>Popularjobs</Text>
-    </View>
-  )
-}
+  const router = useRouter();
+  const isLoading = false;
+  const error = false;
 
-export default Popularjobs
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Popular jobs</Text>
+        <TouchableOpacity>
+          <Text>Show all</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.container}>
+        {isLoading ? (
+          <ActivityIndicator size="large" color={COLORS.primary} />
+        ) : error ? (
+          <Text>Something went wrong</Text>
+        ) : (
+          <FlatList />
+        )}
+      </View>
+    </View>
+  );
+};
+
+export default Popularjobs;
--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,14 +1,76 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import { useState } from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  FlatList,
+  useColorScheme,
+} from 'react-native';
 
-import styles from './welcome.style'
+import { useRouter } from 'expo-router';
+
+import styles from './welcome.style';
+import { COLORS, icons, SIZES } from '../../../constants';
+
+const jobTypes = ['Full-time', 'Part-time', 'Contract'];
 
 const Welcome = () => {
+  const router = useRouter();
+  const colorScheme = useColorScheme();
+  const [activeJobType, setActiveJobType] = useState('Full-time');
+
+  const placeholderColor = colorScheme === 'dark' ? COLORS.dark : COLORS.gray;
+
   return (
     <View>
-      <Text>Welcome</Text>
-    </View>
-  )
-}
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello Andrian</Text>
+        <Text style={styles.welcomeMessage}>Find your perfect job</Text>
+      </View>
 
-export default Welcome
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput
+            style={[styles.searchInput, { color: placeholderColor }]}
+            value=""
+            onChange={() => {}}
+            placeholder="What are you looking for?"
+            placeholderTextColor={placeholderColor}
+          />
+        </View>
+
+        <TouchableOpacity style={styles.searchBtn} onPress={() => {}}>
+          <Image
+            source={icons.search}
+            resizeMode="contain"
+            style={styles.searchBtnImage}
+          />
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.tabsContainer}>
+        <FlatList
+          data={jobTypes}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              style={styles.tab(activeJobType, item)}
+              onPress={() => {
+                setActiveJobType(item);
+                router.push(`./search/${item}`);
+              }}
+            >
+              <Text style={styles.tabText(activeJobType, item)}>{item}</Text>
+            </TouchableOpacity>
+          )}
+          keyExtractor={(item) => item}
+          contentContainerStyle={{ columnGap: SIZES.small }}
+          horizontal
+        />
+      </View>
+    </View>
+  );
+};
+
+export default Welcome;
